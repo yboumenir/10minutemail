15fc1cf2934802aab53009b12daafbf8c3801644
yasser <mboumenir3@gatech.edu>
2024-11-28 07:07:37 -0500

    working now as of 11-28-2024

--- src/minutemail.py
+++ src/minutemail.py
@@ -1,6 +1,8 @@
-import requests
+import json
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from endpoints import NEW_EMAIL, MESSAGE_AFTER, MESSAGE_COUNT
-from fake_useragent import UserAgent
 
 class Mail(object):
     """
@@ -8,11 +10,27 @@ class Mail(object):
     """
 
     def __init__(self):
-        self.session = requests.session()
-        self.session.headers = {'user-agent': UserAgent().random}
+        self.driver = None
         self.message_count = 0
         self.messages = []
-        self.mail = self.session.get(NEW_EMAIL).json()['address']
+        self.mail = None
+        self.start_driver()
+
+    def start_driver(self):
+        options = Options()
+        # options.add_argument("--headless")  # todo make headless
+        options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.set_window_size(1, 1)
+        json_data = self.get_page(NEW_EMAIL)
+        self.mail = json_data['address']
+
+    def get_page(self, url):
+        self.driver.get(url)
+        time.sleep(3)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        json_data = json.loads(soup.find('pre').get_text())
+        return json_data
 
     def get_mail(self):
         """
@@ -31,7 +49,7 @@ class Mail(object):
         Fetches for new messages which are not present in the instance
         :return: List of messages stored in the instance
         """
-        res = self.session.get(MESSAGE_AFTER + str(self.message_count)).json()
+        res = self.get_page(MESSAGE_AFTER + str(self.message_count))
         self.message_count += len(res)
         self.messages += res
         return self.messages
@@ -41,7 +59,18 @@ class Mail(object):
         Check whether there are new messages or not
         :return: bool
         """
-        return self.session.get(MESSAGE_COUNT).json()['messageCount'] != self.message_count
+        return self.get_page(MESSAGE_COUNT)['messageCount'] != self.message_count
+
+    def print_new_messages(self):
+        if self.new_message():
+            bodies = self.fetch_message()
+            for body in bodies:
+                sender = body['sender']
+                subject = body['subject']
+                text = body['bodyPlainText']
+                print(f'from: {sender}')
+                print(f'subject: {subject}')
+                print(f'text: {text}')
 
     def __str__(self):
         return self.mail
@@ -52,4 +81,5 @@ if __name__ == "__main__":
     mail = Mail()
     print(mail.get_mail())
     while True:
+        mail.print_new_messages()
         time.sleep(2)
